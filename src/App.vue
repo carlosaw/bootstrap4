ff47ced7199025e4616a866d161062383205f80a
carlosaw <carlosfreevg@gmail.com>
2020-11-29 18:13:45 -0400

    Primeiro Commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
+    <button class="btn btn-primary">Hola Mundo</button>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>    
   </div>
 </template>
 
